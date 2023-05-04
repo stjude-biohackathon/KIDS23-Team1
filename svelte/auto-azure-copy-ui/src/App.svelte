bfb1d3ef477c0917234a8f6f748f4424fc49c608
William Cole Boren <64283810+williycole@users.noreply.github.com>
2023-05-04 16:46:30 -0500

    initial svelte/ts commit and minimal component setup

---
+++ svelte/auto-azure-copy-ui/src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import ManageOrderContainer from "./lib/ManageOrderContainer.svelte";
+</script>
+
+<main>
+  <div class="order-components-container">
+    <ManageOrderContainer title="Place Order">
+      <!-- TODO - place order section -->
+      <div>hey from place order inside app.svelte</div>
+    </ManageOrderContainer>
+    <ManageOrderContainer title ="Order Info">
+      <!-- TODO order info section -->
+      <div>hey from order info inside app.svelte</div>
+    </ManageOrderContainer>
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ svelte/auto-azure-copy-ui/src/lib/ManageOrderContainer.svelte
@@ -0,0 +1,15 @@
+
+<script lang="ts">
+    export let title: string;
+  </script>
+
+  <div class ="manage-order-container">
+    <h1>{title}</h1>
+    <slot></slot>
+  </div>
+
+  <style>
+    div {
+      /* Your styles here */
+    }
+  </style>
