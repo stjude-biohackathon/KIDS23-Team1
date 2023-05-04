a22e3c5dffde1a997f23404f76f3997a92099357
Wayne W <zwang63@stjude.org>
2023-05-04 13:37:49 -0500

    routes

---
+++ control/control.go
@@ -0,0 +1,29 @@
+package control
+
+// get user related info
+func Get_user_by_id(user_id int) {
+	print("Hello World fronm order controller!")
+}
+
+// get sample related info
+func Get_sample_and_related_files(sample_id int) {
+	print("Hello World fronm order controller!")
+}
+
+// get order related info
+func Get_orders_by_user_id(user_id int) {
+	print("Hello World fronm order controller!")
+}
+
+// order operations
+func Submit_order(user_id int, sample_id int, order_id int) {
+	print("Hello World fronm order controller!")
+}
+
+func Cancel_order(user_id int, order_id int) {
+	print("Hello World fronm order controller!")
+}
+
+func Modify_order(order_id int) {
+	print("Hello World fronm order controller!")
+}
