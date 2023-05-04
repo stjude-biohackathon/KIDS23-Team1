27d6e64a26391e5ad3c3ab8641c6f0e620a425c7
Wayne W <zwang63@stjude.org>
2023-05-04 11:18:44 -0500

    api is running

---
+++ API/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"runtime"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	router.GET("/os", func(c *gin.Context) {
+		c.String(200, runtime.GOOS)
+	})
+	router.Run(":8383")
+}
